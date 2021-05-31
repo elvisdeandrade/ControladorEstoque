2e458c558768337620d0aa695bdc3fc1443e02b0
elvis <elvisdeandrade@outlook.com>
2021-05-31 19:20:17 -0300

    setup webapi

---
+++ ControladorDeEstoque/Controllers/ClienteController.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using ControladorDeEstoque.Controllers.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+
+namespace ControladorDeEstoque.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class ClienteController : ControllerBase
+    {
+        private static readonly Cliente[] Clientes = new[]
+        {
+           new Cliente{
+               Id=1,
+               Nome="Marcio",
+               Cpf="35805880520"
+           }
+        };
+
+        private readonly ILogger<ClienteController> _logger;
+
+        public ClienteController(ILogger<ClienteController> logger)
+        {
+            _logger = logger;
+        }
+
+        [HttpGet]
+        public IEnumerable<Cliente> Get()
+        {
+            return Clientes;
+        }
+    }
+}
---
+++ ControladorDeEstoque/Controllers/Models/Cliente.cs
@@ -0,0 +1,11 @@
+namespace ControladorDeEstoque.Controllers.Models
+{
+    public class Cliente
+    {
+        public int Id { get; set; }
+
+        public string Nome { get; set; }
+
+        public string Cpf { get; set; }
+    }
+}
